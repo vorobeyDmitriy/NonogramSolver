f8d455ca948f14a3ac2ec6bc3f43c03a138cc864
Dmitriy <dmitriy.vorobey1@gmail.com>
2021-03-06 14:35:31 +0300

    code clean up

--- src/NonogramSolver.Core/Interfaces/ICellsService.cs
+++ src/NonogramSolver.Core/Interfaces/ICellsService.cs
@@ -8,7 +8,7 @@ namespace NonogramSolver.Core.Interfaces
         void CrossCells(IEnumerable<Cell> cells);
 
         void FillNumber(List<Cell> cells, LineNumber number, int startIndex, bool withResolve = true);
-        
+
         void ResolveNumbers(IEnumerable<LineNumber> numbers);
     }
 }
